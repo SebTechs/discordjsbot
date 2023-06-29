f24af39ca6515c028381562e799e5eaa8386d106
Seb <sjalowiecki2@gmail.com>
2023-06-29 15:00:52 +0100

    Vital Update to make sure that messages can be sent

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const {
+    Client,
+    Events,
+    GatewayIntentBits,
+    IntentsBitField,
+} = require("discord.js");
 const { token } = require("./config.json");
 
 //Little error handler
 if (token === "Please enter your bot token here!") {
     console.log("Enter your bot token in config.json before starting.");
-} else {
+}
+else{
+
+
     // Create a new client instance
-    const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+    const client = new Client({
+        intents: [
+            IntentsBitField.Flags.Guilds,
+            IntentsBitField.Flags.GuildMembers,
+            IntentsBitField.Flags.GuildMessages,
+            IntentsBitField.Flags.MessageContent,
+        ],
+    });
 
     // When the client is ready, run this code (only once)
     // We use 'c' for the event parameter to keep it separate from the already defined 'client'
     client.once(Events.ClientReady, (c) => {
-        console.log(`Ready! Logged in as ${c.user.tag}`);
+        console.log("Ready! Logged in as ${c.user.tag}");
+    });
+
+    client.on("messageCreate", (messsage) => {
+        console.log(messsage.content);
+        console.log(messsage.author.id);
+        console.log(typeof messsage.author.id);
     });
 
     // Log in to Discord with your client's token
